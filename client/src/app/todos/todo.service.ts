e29e534057bc31be0ed7306934c1f5c50db63e1a
Alex <trinc005@morris.umn.edu>
2023-03-01 12:21:57 -0600

    Changed Modules Updated AppRouting, added to Todo modules.

---
+++ client/src/app/todos/todo.service.ts
@@ -0,0 +1,63 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { Todo } from './todo';
+
+
+@Injectable()
+export class TodoService {
+
+  readonly todoUrl: string = environment.apiUrl + 'todos';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getTodos(filters?: { owner?: string; status?: boolean; body?: string; category?: string }): Observable<Todo[]> {
+
+    let httpParams: HttpParams = new HttpParams();
+    if (filters) {
+      if (filters.owner) {
+        httpParams = httpParams.set('owner', filters.owner);
+      }
+      if (filters.status) {
+        httpParams = httpParams.set('status', filters.status);
+      }
+      if (filters.body) {
+        httpParams = httpParams.set('body', filters.body);
+      }
+      if (filters.category) {
+        httpParams = httpParams.set('category', filters.category);
+      }
+    }
+
+    return this.httpClient.get<Todo[]>(this.todoUrl, {
+      params: httpParams,
+    });
+  }
+
+  getTodosById(id: string): Observable<Todo> {
+    return this.httpClient.get<Todo>(this.todoUrl + '/' + id);
+  }
+
+  filterTodos(todos: Todo[], filters: { owner?: string; body?: string; category?: string }): Todo[] {
+    let filteredTodos = todos;
+
+    if (filters.owner) {
+      filters.owner = filters.owner.toLowerCase();
+      filteredTodos = filteredTodos.filter(todo => todo.owner.toLowerCase().indexOf(filters.owner) !== -1);
+    }
+
+    if (filters.body) {
+      filters.body = filters.body.toLowerCase();
+      filteredTodos = filteredTodos.filter(todo => todo.body.toLowerCase().indexOf(filters.body) !== -1);
+    }
+
+    if (filters.category) {
+      filters.category = filters.category.toLowerCase();
+      filteredTodos = filteredTodos.filter(todo => todo.category.toLowerCase().indexOf(filters.category) !== -1);
+    }
+
+    return filteredTodos;
+  }
+}
